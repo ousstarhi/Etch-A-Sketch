19d745f931231ad0ffbf54d41d612fdf1911bc7b
ousstarhi <ousstarhi.web@gmail.com>
2020-01-14 20:07:50 +0100

    create grid of squares with prompt()

---
+++ index.js
@@ -0,0 +1,10 @@
+const promptSize = prompt('Enter a number');
+const gridSize = +promptSize; // Return a number from prompt()
+
+// Create the grid fo square
+for (let i = 0; i < (gridSize ** 2); i += 1) {
+  const mainContainer = document.getElementById('main-container');
+  mainContainer.setAttribute('style', `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr)`);
+  const squareDiv = document.createElement('div');
+  mainContainer.appendChild(squareDiv);
+}
