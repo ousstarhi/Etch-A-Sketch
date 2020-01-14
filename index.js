86945e9cd972407f602346634689befc7c0c8929
ousstarhi <ousstarhi.web@gmail.com>
2020-01-14 20:08:34 +0100

    correct a comment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const promptSize = prompt('Enter a number');
 const gridSize = +promptSize; // Return a number from prompt()
 
-// Create the grid fo square
+// Create the grid of square
 for (let i = 0; i < (gridSize ** 2); i += 1) {
   const mainContainer = document.getElementById('main-container');
   mainContainer.setAttribute('style', `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr)`);
